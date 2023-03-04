f230d8366da203d262caf96194e25409ed7d4fc0
Ceffrece <cheffrece@quinnipiac.edu>
2023-03-04 12:19:46 -0500

    Fixed issue with already having the time at 200 committed

--- DmvSimulator.java
+++ DmvSimulator.java
@@ -3,12 +3,12 @@ public class DmvSimulator{
     public static void main(String[] args) throws InterruptedException{
         System.out.println("Welcome to the DMV!");
         SecureRandom randomNum = new SecureRandom();
-        int ticketNum = randomNum.nextInt(200);
+        int ticketNum = randomNum.nextInt(100);
         System.out.println("Your ticket number is " + ticketNum + ". Please wait until your number is called. Thank you.");
         int count = ticketNum + 1;
         while(count != ticketNum){
             System.out.println("Calling Number " + count);
-            if(count == 200){
+            if(count == 100){
                 count = 0;
             }
             Thread.sleep(1000); //Comment out this line before running unless you want a stupid long wait
